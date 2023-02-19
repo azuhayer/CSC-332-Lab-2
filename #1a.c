91a68a26a41ae0edd83db194244bb7c5bff7eb21
Zuhayer Alvi <azuhayer@gmail.com>
2023-02-19 11:23:21 -0500

    Update #1a.c

---
+++ #1a.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char* argv[])
+{
+    char* filepath = argv[1];
+    int returnval;
+
+    //check file existence
+    returnval = access (filepath, F_OK);
+    if(returnval == 0)
+        printf("\n %s exists\n", filepath);
+    else
+    {
+        if(errno == ENOENT)
+            printf("%s does not exist\n", filepath);
+        else if (errno == EACCES)
+            printf("%s is not accessible\n", filepath);
+        return 0;
+    }
+
+    // Check read access
+    returnval = access(filepath, R_OK);
+
+    if(returnval == 0) 
+        printf("%s has access to read permissions.", filepath);
+    else 
+    {
+        if(errno == ENOENT) 
+            printf("%s There is no such file in directory.\n", filepath);
+        else if(errno == EACCES) 
+            printf ("%s Read permissions have been denied.\n", filepath);
+        return 0;
+    }
+
+    // Check write access
+    returnval = access(filepath, W_OK);
+    if(returnval == 0) 
+    printf("\n %s has access to write permissions.", filepath);
+    else 
+    {
+        if(errno == ENOENT) 
+            printf("%s There is no such file in directory.\n", filepath);
+        else if(errno == EACCES) 
+            printf ("%s Write permissions have been denied.\n", filepath);
+        return 0;
+    }
+    return 0;
+}
