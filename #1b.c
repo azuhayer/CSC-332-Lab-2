bbd7d4e7ad12e7bd0e896219c4537343b343b148
Zuhayer Alvi <azuhayer@gmail.com>
2023-02-19 11:34:04 -0500

    Update #1b.c

---
+++ #1b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main (int argc, char* argv[]) 
+{
+    char* filePath = argv[1];
+    int returnValue;
+    returnValue = open(filePath, O_CREAT | O_RDWR, 00777);
+    if(returnValue == -1) 
+    {
+        printf("\n Error: %d\n", errno);
+    }
+    else 
+    {
+        printf("\n File has been successfully opened!\n");
+    }
+    return 0;
+}
